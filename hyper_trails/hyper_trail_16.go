12a3314e60440af2384d7ec103ba4731b096879f
Artsiom Trubchyk <aglitchman@gmail.com>
2019-04-01 15:17:51 +0300

    Remove old models, objects etc.

---
+++ hyper_trails/hyper_trail_16.go
@@ -0,0 +1,39 @@
+components {
+  id: "trail_maker"
+  component: "/hyper_trails/trail_maker.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "trail_model"
+  type: "model"
+  data: "mesh: \"/hyper_trails/models/trail_16.dae\"\n"
+  "material: \"/hyper_trails/materials/trail.material\"\n"
+  "textures: \"/hyper_trails/textures/texture0_1.png\"\n"
+  "textures: \"/hyper_trails/textures/white.png\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- hyper_trails/materials/trail.fp
+++ hyper_trails/materials/trail.fp
@@ -9,5 +9,4 @@ void main()
     // Pre-multiply alpha since all runtime textures already are
     lowp vec4 tint_pm = vec4(var_tint.xyz * var_tint.w, var_tint.w);
     gl_FragColor = texture2D(texture1, var_texcoord0.xy) * tint_pm;
-    // gl_FragColor = tint_pm;
 }
