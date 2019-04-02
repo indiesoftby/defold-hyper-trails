2572a2aef9918fd78e5d9ee7b40c3841c428e88a
Artsiom Trubchyk <aglitchman@gmail.com>
2019-04-02 20:06:50 +0300

    Add trail model with 32 segments.

---
+++ hyper_trails/hyper_trail_32.go
@@ -0,0 +1,44 @@
+components {
+  id: "trail_maker"
+  component: "/hyper_trails/trail_maker.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "segments_count"
+    value: "32.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+}
+embedded_components {
+  id: "trail_model"
+  type: "model"
+  data: "mesh: \"/hyper_trails/models/trail_32.dae\"\n"
+  "material: \"/hyper_trails/materials/trail.material\"\n"
+  "textures: \"/hyper_trails/textures/texture0_2.png\"\n"
+  "textures: \"/hyper_trails/textures/white.png\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
