dc6c0c060d6a86fff9e96e0a3d5f03fb4c2fa83e
Artsiom Trubchyk <aglitchman@gmail.com>
2019-04-02 17:17:50 +0300

    Add third sprite and trail that follows cursor.

--- hyper_trails/msgs.lua
+++ hyper_trails/msgs.lua
@@ -3,5 +3,6 @@ local M = {}
 M.FOLLOW_POSITION = hash("follow_position")
 M.FOLLOW_TARGET = hash("follow_target")
 M.RESET_POSITION = hash("reset_position")
+M.DRAW_TRAIL = hash("draw_trail")
 
 return M
